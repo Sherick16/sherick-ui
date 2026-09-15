ae224734943961d1ffea608144897dedc4d0b60a
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 14:25:13 +0200

    apply shared focus ring to icon buttons

--- components/UI/IconButton.tsx
+++ components/UI/IconButton.tsx
@@ -2,7 +2,7 @@
 
 import React, { forwardRef, type ButtonHTMLAttributes, type ReactNode } from "react";
 import { cn } from "@/libs/utils";
-import { styleMap } from "./ui.common";
+import { focusRing, styleMap } from "./ui.common";
 import { Variant } from "./ui.types";
 import { Spinner } from "./Spinner";
 
@@ -24,6 +24,7 @@ const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
         aria-busy={loading || undefined}
         className={cn(
           "p-2 rounded-2xl bg-opacity-0 hover:bg-opacity-40 transition-all flex items-center justify-center",
+          focusRing,
           styleMap[variant] || styleMap.primary,
           isDisabled ? "cursor-not-allowed opacity-60" : "cursor-pointer",
           className
