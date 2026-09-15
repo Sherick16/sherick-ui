c1a33c7cdae0bd35d9d1554818dc48c373184c84
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 14:55:54 +0200

    refine navigation item styling

--- components/UI/NavItem.tsx
+++ components/UI/NavItem.tsx
@@ -1,6 +1,6 @@
 import React, { type AnchorHTMLAttributes, type ReactNode } from "react";
 import { cn } from "@/libs/utils";
-import { focusRing } from "./ui.common";
+import { focusRing, motionState, pressable } from "./ui.common";
 
 export interface NavItemProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   children: ReactNode;
@@ -12,14 +12,16 @@ const NavItem = ({ children, icon, className, to, ...props }: NavItemProps) => (
   <a
     href={to}
     className={cn(
-      "right-2 relative max-w-xs block overflow-hidden whitespace-nowrap text-ellipsis text-sm text-gray-200 hover:bg-gray-700 hover:bg-opacity-40 p-2 rounded-3xl",
+      "flex min-h-10 max-w-xs items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap rounded-xl px-3 py-2 text-sm text-zinc-300 hover:bg-white/5 hover:text-zinc-100 active:bg-white/10",
+      motionState,
       focusRing,
+      pressable,
       className
     )}
     {...props}
   >
-    {icon && <span className="mr-2 inline-block align-middle">{icon}</span>}
-    {children}
+    {icon && <span className="inline-flex shrink-0 items-center" aria-hidden="true">{icon}</span>}
+    <span className="truncate">{children}</span>
   </a>
 );
 
