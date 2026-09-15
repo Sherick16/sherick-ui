c1826fe3d11389a62e38f8de9d0cce8f8a3a8908
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 14:16:48 +0200

    make textareas borderless

--- components/UI/Textarea.tsx
+++ components/UI/Textarea.tsx
@@ -39,10 +39,10 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
           required={required}
           aria-invalid={error || undefined}
           className={cn(
-            "px-4 py-4 rounded-lg bg-opacity-20 hover:bg-opacity-40 border transition-all min-w-64 min-h-16 w-full border-opacity-20 focus:outline-none focus:border-opacity-50",
+            "px-4 py-4 rounded-lg bg-opacity-20 hover:bg-opacity-40 transition-all min-w-64 min-h-16 w-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white/20",
             !error
-              ? "border-gray-400 bg-gray-400 text-gray-300 hover:bg-gray-500"
-              : "border-red-500 border-opacity-40 focus:border-opacity-100 text-red-400 bg-red-500 bg-opacity-10 hover:bg-opacity-20 hover:bg-red-500",
+              ? "bg-gray-400 text-gray-300 hover:bg-gray-500"
+              : "text-red-300 bg-red-500 bg-opacity-15 hover:bg-opacity-25 hover:bg-red-500 focus-visible:ring-red-400/30",
             textareaClassName
           )}
           onChange={(event) => onChange?.(event.target.value)}
