01ef4cf4ea80a4741a8d0dc75d076c638fd0505a
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 14:55:13 +0200

    refine modal content rhythm

--- components/UI/ModalContent.tsx
+++ components/UI/ModalContent.tsx
@@ -8,7 +8,10 @@ export interface ModalContentProps {
 
 export const ModalContent = ({ children, className }: ModalContentProps) => {
   return (
-    <div id="modal-description" className={cn("px-6 py-4 text-gray-300", className)}>
+    <div
+      id="modal-description"
+      className={cn("px-6 py-4 text-sm leading-7 text-zinc-300 sm:px-7", className)}
+    >
       {children}
     </div>
   );
