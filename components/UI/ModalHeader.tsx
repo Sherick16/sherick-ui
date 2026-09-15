58d8bff38d60eb21afd39d03e9fd5f7cfa3a3586
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 16:12:42 +0200

    tighten modal header spacing

--- components/UI/ModalHeader.tsx
+++ components/UI/ModalHeader.tsx
@@ -8,8 +8,8 @@ export interface ModalHeaderProps {
 
 export const ModalHeader = ({ children, className }: ModalHeaderProps) => {
   return (
-    <div className={cn("px-6 pb-3 pt-7 pr-20 sm:px-7 sm:pt-8", className)}>
-      <div id="modal-title" className="text-2xl font-semibold tracking-[-0.025em] text-sherick-ink">
+    <div className={cn("px-6 pb-2 pt-6 pr-18 sm:px-7", className)}>
+      <div id="modal-title" className="text-xl font-semibold tracking-[-0.02em] text-sherick-ink">
         {children}
       </div>
     </div>
