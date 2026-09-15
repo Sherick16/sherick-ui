aa606cf9fe09a3ad11f2f46a6f745c01bbb6e6e1
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 14:49:59 +0200

    align skeleton with surface language

--- components/UI/Skeleton.tsx
+++ components/UI/Skeleton.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { cn } from "@/libs/utils";
+import { shape } from "./ui.common";
 
 export const Skeleton = ({ className, ...props }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "animate-pulse bg-gray-500 bg-opacity-20 rounded-4xl",
+        "animate-pulse bg-zinc-700/45 motion-reduce:animate-none",
+        shape.control,
         className
       )}
       {...props}
