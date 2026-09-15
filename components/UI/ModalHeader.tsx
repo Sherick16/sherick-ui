a0f7db77f3b038c4ddd8f72878e7e7cff835a02e
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-15 16:12:52 +0200

    fix modal header padding utility

--- components/UI/ModalHeader.tsx
+++ components/UI/ModalHeader.tsx
@@ -8,7 +8,7 @@ export interface ModalHeaderProps {
 
 export const ModalHeader = ({ children, className }: ModalHeaderProps) => {
   return (
-    <div className={cn("px-6 pb-2 pt-6 pr-18 sm:px-7", className)}>
+    <div className={cn("px-6 pb-2 pt-6 pr-20 sm:px-7", className)}>
       <div id="modal-title" className="text-xl font-semibold tracking-[-0.02em] text-sherick-ink">
         {children}
       </div>
