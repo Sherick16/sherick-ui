afc966e851467651f0c760dec1c99d547f40c370
Sherick16 <ajaxsdvbc3to@Gmail.com>
2026-09-16 15:41:29 +0200

    Scope Sherick component class recipes

--- packages/ui/src/libs/utils.ts
+++ packages/ui/src/libs/utils.ts
@@ -1,11 +1,17 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { SUI_SCOPE_CLASS } from "../styles/scope";
 
 /** Timer handle: a `number` in the DOM, an object under Node typings. */
 export type TimerHandle = ReturnType<typeof setTimeout>;
 
+/**
+ * Component class composer. The internal scope marker lets the published stylesheet
+ * carry Tailwind-authored rules without leaking generic utility selectors into the
+ * consumer application.
+ */
 export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(SUI_SCOPE_CLASS, inputs));
 };
 
 export const capitalize = (str: string) => {
